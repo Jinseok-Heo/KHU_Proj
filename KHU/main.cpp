04f0a0d7f7d63657fca12b7e4b6f8888921ba57b
Jinseok <hjs7747@naver.com>
2021-03-10 09:55:18 +0900

    Initial Commit

---
+++ KHU/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KHU
+//
+//  Created by Jinseok on 2021/03/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
